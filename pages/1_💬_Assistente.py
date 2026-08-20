3af0b92b20ed027df883be90ec54fb50d5226953
Sergio Luiz Brito <sergiolmendes2026@gmail.com>
2026-08-19 22:44:57 -0300

    Update 1_💬_Assistente.py

--- pages/1_💬_Assistente.py
+++ pages/1_💬_Assistente.py
@@ -1,6 +1,9 @@
 import os
 import tempfile
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 from groq import Groq
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
